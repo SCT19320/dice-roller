55ed0d62b220b1d2f8c3fe43a6efac64d4630d6b
Shane <shane9596@protonmail.com>
2022-05-28 13:33:17 +0100

    Removed unnecessary variable and tidied code

--- dice-roller-launcher.js
+++ dice-roller-launcher.js
@@ -1,8 +1,5 @@
-var lastRollNumber;
-
 function diceRoller(){
     var diceNumber = Math.floor(Math.random() * 6) + 1;
     var pngSelector = './Assets/dice' + diceNumber + '.png';
     document.querySelectorAll('img')[0].setAttribute('src', pngSelector);
-    lastRollNumber = diceNumber;
 }
